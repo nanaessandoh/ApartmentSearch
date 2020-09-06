11fc0c166a5c68e1281d1661a1203a5167447167
Nana Essandoh <nanaessandoh@gmail.com>
2020-09-06 14:43:53 -0230

    Add project files.

---
+++ ApartmentSearch/Data/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApartmentSearch.Data
+{
+    public class ApplicationUser
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
